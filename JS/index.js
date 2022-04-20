a108608c285d6efcc08a649bb8be0a626524e88f
akbar <akbarsmp27@gmail.com>
2022-04-21 05:00:18 +0700

    telah menghapus bug ternyat kurang event onmouseout.

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+const homeBtn = document.querySelector('.home-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const followBtn = document.querySelector('.follow-btn');
+homeBtn.addEventListener('mouseover', e => {
+    let target = e.target;
+    if (e.target == homeBtn) {
+        target.childNodes[1].classList.remove('hidden');
+        target.childNodes[1].classList.add('flex');
+    }
+});
+homeBtn.addEventListener('mouseout', e => {
+    let target = e.target;
+    if (e.target == homeBtn) {
+        target.childNodes[1].classList.remove('flex');
+        target.childNodes[1].classList.add('hidden');
+    }
+});
+followBtn.addEventListener('mouseover', e => {
+    let target = e.target;
+    if (e.target == followBtn) {
+        target.childNodes[1].classList.remove('hidden');
+        target.childNodes[1].classList.add('flex');
+    }
+});
+followBtn.addEventListener('mouseout', e => {
+    let target = e.target;
+    if (e.target == followBtn) {
+        target.childNodes[1].classList.remove('flex');
+        target.childNodes[1].classList.add('hidden');
+    }
+});
+aboutBtn.addEventListener('mouseover', e => {
+    let target = e.target;
+    if (e.target == aboutBtn) {
+        target.childNodes[1].classList.remove('hidden');
+        target.childNodes[1].classList.add('flex');
+    }
+});
+aboutBtn.addEventListener('mouseout', e => {
+    let target = e.target;
+    if (e.target == aboutBtn) {
+        target.childNodes[1].classList.remove('flex');
+        target.childNodes[1].classList.add('hidden');
+    }
+});
--- TS/index.ts
+++ TS/index.ts
@@ -1,1 +1,51 @@
-console.log("hello world")
+const homeBtn = document.querySelector('.home-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const followBtn = document.querySelector('.follow-btn');
+
+homeBtn.addEventListener('mouseover', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == homeBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('hidden');
+        (target.childNodes[1] as HTMLDivElement).classList.add('flex')
+    }
+})
+
+homeBtn.addEventListener('mouseout', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == homeBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('flex');
+        (target.childNodes[1] as HTMLDivElement).classList.add('hidden')
+    }
+})
+
+followBtn.addEventListener('mouseover', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == followBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('hidden');
+        (target.childNodes[1] as HTMLDivElement).classList.add('flex')
+    }
+})
+
+followBtn.addEventListener('mouseout', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == followBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('flex');
+        (target.childNodes[1] as HTMLDivElement).classList.add('hidden')
+    }
+})
+
+aboutBtn.addEventListener('mouseover', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == aboutBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('hidden');
+        (target.childNodes[1] as HTMLDivElement).classList.add('flex')
+    }
+})
+
+aboutBtn.addEventListener('mouseout', e => {
+    let target = (e.target as HTMLDivElement);
+    if(e.target == aboutBtn){
+        (target.childNodes[1] as HTMLDivElement).classList.remove('flex');
+        (target.childNodes[1] as HTMLDivElement).classList.add('hidden')
+    }
+})
