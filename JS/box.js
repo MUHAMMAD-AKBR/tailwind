bc031d63a996a09817749541eaa9008ff622c4e5
akbar <akbarsmp27@gmail.com>
2022-04-25 20:38:04 +0700

    menambahkan content pada dialogbox sesuai tombol c e dan o.

--- JS/box.js
+++ JS/box.js
@@ -2,14 +2,23 @@ export const canvas = document.querySelector('.canvas');
 export const ecom = document.querySelector('.ecom');
 export const other = document.querySelector('.other');
 const dialogbox = document.querySelector('.dialogbox');
+let c_text = `<div class="grid-canvas grid grid-cols-2 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer"> Matrix</div>
+             <div class="grid-itemcanvas  border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer"> Falling balls</div>
+    </div>`;
+let e_text = ` <div class="grid-canvas grid grid-cols-1 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer">Cart Feature</div>
+</div>`;
+let o_text = `<div class="grid-canvas grid grid-cols-1 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer">Other...</div>
+</div>`;
 export function cHandler() {
     dialogbox.classList.toggle('opacity-0');
     dialogbox.classList.add('animate-pop');
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop');
     }, 400);
-    let content = canvas.textContent;
-    dialogbox.textContent = content;
+    dialogbox.innerHTML = c_text;
 }
 export function eHandler() {
     dialogbox.classList.toggle('opacity-0');
@@ -17,8 +26,7 @@ export function eHandler() {
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop');
     }, 400);
-    let content = ecom.textContent;
-    dialogbox.textContent = content;
+    dialogbox.innerHTML = e_text;
 }
 export function oHandler() {
     dialogbox.classList.toggle('opacity-0');
@@ -26,6 +34,5 @@ export function oHandler() {
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop');
     }, 400);
-    let content = other.textContent;
-    dialogbox.textContent = content;
+    dialogbox.innerHTML = o_text;
 }
--- TS/box.ts
+++ TS/box.ts
@@ -3,14 +3,27 @@ export const ecom = document.querySelector('.ecom') as HTMLDivElement;
 export const other = document.querySelector('.other') as HTMLDivElement;
 const dialogbox = document.querySelector('.dialogbox') as HTMLDivElement;
 
+let c_text = `<div class="grid-canvas grid grid-cols-2 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer"> Matrix</div>
+             <div class="grid-itemcanvas  border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer"> Falling balls</div>
+    </div>`;
+
+
+let e_text = ` <div class="grid-canvas grid grid-cols-1 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer">Cart Feature</div>
+</div>`;
+
+let o_text = `<div class="grid-canvas grid grid-cols-1 gap-2">
+            <div class="grid-itemcanvas border border-green-400 rounded-sm p-2 font-header text-lg text-center cursor-pointer">Other...</div>
+</div>`
+
 export function cHandler(){
     dialogbox.classList.toggle('opacity-0');
     dialogbox.classList.add('animate-pop');
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop')
-    }, 400)
-    let content = canvas.textContent;
-    dialogbox.textContent = content;
+    }, 400) 
+    dialogbox.innerHTML = c_text;
 }
 
 export function eHandler(){
@@ -18,9 +31,8 @@ export function eHandler(){
     dialogbox.classList.add('animate-pop');
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop')
-    }, 400)
-    let content = ecom.textContent;
-    dialogbox.textContent = content;
+    }, 400) 
+    dialogbox.innerHTML = e_text;
 }
 
 export function oHandler(){
@@ -29,6 +41,5 @@ export function oHandler(){
     setTimeout(() => {
         dialogbox.classList.remove('animate-pop')
     }, 400)
-    let content = other.textContent;
-    dialogbox.textContent = content;
+    dialogbox.innerHTML = o_text;
 }
