f3a07fc31bda03d6b5601c3a00db4d0967f2364d
akbar <akbarsmp27@gmail.com>
2022-05-01 12:52:39 +0700

    memindahkan index.html ke dalam root.

--- routing.js
+++ routing.js
@@ -1,9 +1,6 @@
 const app = require('express')();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.header(200)
-    res.sendFile("index.html", {root: "./src/"})
-})
+
 
 app.listen(PORT,() => console.log(`listening on http://localhost:${PORT}`))
